ca5356f44d7a8b4f189f7016650d401789b7b476
Gar|k <garik.djan@gmail.com>
2017-03-21 15:25:30 +0500

    add tabs

--- test.js
+++ test.js
@@ -21,131 +21,11 @@ var template = Twig.twig({
     </section>'
 });
 
-
-
-
-/*
-function parseToken(token) {
-
-    var output = [],
-        type = 'raw',
-        arr = [];
-
-    switch (token.type) {
-
-        case 'Twig.expression.type.number':
-
-            output += token.value;
-            break;
-        case 'Twig.expression.type.key.brackets':
-            output += '[';
-            arr = [];
-
-            for (var i = 0; i < token.stack.length; i++) {
-                arr.push(parseToken(token.stack[i]));
-            }
-            output += toSource(arr) + ']';
-            break;
-        case 'Twig.logic.type.set':
-            output += '$' + token.key + ' = ';
-            arr = [];
-            for (var i = 0; i < token.expression.length; i++) {
-                arr.push(parseToken(token.expression[i]));
-            }
-            output += toSource(arr) + ';';
-            break;
-
-        case 'Twig.expression.type.string':
-            output += "'" + token.value + "'";
-            break;
-        case 'Twig.expression.type.parameter.start':
-        case 'Twig.expression.type.parameter.end':
-            output += token.value;
-            break;
-        case 'Twig.expression.type._function':
-
-            output += token.fn;
-            arr = [];
-            for (var i = 0; i < token.params.length; i++) {
-                arr.push(parseToken(token.params[i]));
-            }
-
-            output += toSource(arr);
-            break;
-        case 'Twig.expression.type.key.period':
-            output += "['" + token.key + "']";
-            break;
-
-        case 'Twig.logic.type.if':
-            output += "if(";
-            arr = [];
-            for (var i = 0; i < token.stack.length; i++) {
-                arr.push(parseToken(token.stack[i]));
-            }
-            output += toSource(arr) + ")";
-
-            output += " {\n";
-            arr = [];
-            for (var i = 0; i < token.output.length; i++) {
-                arr.push(parseToken(token.output[i]));
-            }
-            output += toSource(arr) + "}\n";
-            break;
-        case 'Twig.logic.type.for':
-
-            var as = ' as ';
-            if (token.key_var && token.value_var) {
-                as += '$' + token.key_var + ' => ';
-            }
-            as += '$' + token.value_var;
-
-            output += 'foreach (';
-            for (var i = 0; i < token.expression.length; i++) {
-                output += parseToken(token.expression[i]).value;
-            }
-            output += as + ')';
-
-            output += " {\n";
-            arr = [];
-            for (var i = 0; i < token.output.length; i++) {
-                arr.push(parseToken(token.output[i]));
-            }
-            output += toSource(arr) + "}\n";
-
-
-
-            break;
-        case 'raw':
-            type = 'echo';
-            output += '"' + token.value + '"';
-            break;
-        case 'Twig.expression.type.variable':
-            type = 'var';
-            output += '$' + token.value;
-            break;
-        case 'logic':
-            output += parseToken(token.token).value;
-            break;
-        case 'output':
-            arr = [];
-            for (var i = 0; i < token.stack.length; i++) {
-                arr.push(parseToken(token.stack[i]));
-            }
-            output += toSource(arr);
-            break;
-        default:
-            //console.log(token);
-    }
-
-    //console.log(type, output);
-    return {
-        type: type,
-        value: output
-    };
-}*/
-
 function createToken(input, type, value) {
-  input.push({ type: type, value:value });
+    input.push({
+        type: type,
+        value: value
+    });
 }
 
 function parseToken(token) {
@@ -172,7 +52,7 @@ function parseToken(token) {
 
             createToken(output, 'set', '$' + token.key + ' = ');
             for (var i = 0; i < token.expression.length; i++) {
-                  output.push(parseToken(token.expression[i]));
+                output.push(parseToken(token.expression[i]));
             }
 
             createToken(output, type, ";\n");
@@ -222,7 +102,7 @@ function parseToken(token) {
             }
             as += '$' + token.value_var;
 
-            createToken(output,'foreach', 'foreach (');
+            createToken(output, 'foreach', 'foreach (');
             for (var i = 0; i < token.expression.length; i++) {
                 output.push(parseToken(token.expression[i]));
             }
@@ -239,8 +119,8 @@ function parseToken(token) {
             var s = token.value.toString().trim().replace(/\'/g, '\\\'');
             s = s.replace(/[\s\uFEFF\xA0]{2,}/g, '');
 
-            if(s.length > 0) {
-              createToken(output,'echo', "'" + s + "'");
+            if (s.length > 0) {
+                createToken(output, 'echo', "'" + s + "'");
             }
             break;
         case 'Twig.expression.type.variable':
@@ -269,7 +149,8 @@ for (var i = 0; i < template.tokens.length; i++) {
 }
 
 var flags = {
-    echo: false
+    echo: false,
+    tabs: 0
 };
 
 function toSource(items) {
@@ -280,64 +161,66 @@ function toSource(items) {
     for (var i = 0; i < items.length; i++) {
         var item = items[i];
 
+        if (Array.isArray(item)) {
 
-
-        if(Array.isArray(item)) {
-
-          source_code += toSource(item);
-          continue;
+            source_code += toSource(item);
+            continue;
         }
 
         switch (item.type) {
-          case 'foreach':
-          case 'if':
-          case 'set':
-          case 'ebrace':
-            if(flags.echo) {
-              flags.echo = false;
-              source_code += ";\n";
-            }
-            break;
-          case '_function':
-            if(flags.echo) {
-              source_code += ' . ';
-            }
-            break;
-          case 'var':
-            if(flags.echo) {
-              source_code += ' . ';
-            }
-            break;
-          case 'echo':
-              if(!flags.echo) {
-                flags.echo = true;
-                source_code += 'echo ';
-              }
-              else {
-                source_code += ' . ';
-              }
-            break;
-          default:
-            //console.log(item, flags);
+            case 'ebrace':
+                --flags.tabs;
+            case 'set':
+                source_code += "\t".repeat(flags.tabs);
+            case 'foreach':
+            case 'if':
+                if (flags.echo) {
+                    flags.echo = false;
+                    source_code += ";\n" + "\t".repeat(flags.tabs);
+                }
+                break;
+            case '_function':
+                if (flags.echo) {
+                    source_code += ' . ';
+                }
+                break;
+            case 'var':
+                if (flags.echo) {
+                    source_code += ' . ';
+                }
+                break;
+            case 'echo':
+                if (!flags.echo) {
+                    flags.echo = true;
+                    source_code += "\t".repeat(flags.tabs) + 'echo ';
+                } else {
+                    source_code += ' . ';
+                }
+                break;
+            default:
+                //console.log(item, flags);
         }
 
         source_code += item.value;
 
         switch (item.type) {
-          case 'obrace':
-          case 'ebrace':
-            source_code += "\n";
-          break;
+            case 'obrace':
+                flags.tabs++;
+            case 'ebrace':
+                source_code += "\n";
+                break;
         }
+
+
 
     };
 
     return source_code;
 }
 
-var source = '<?php ' + toSource(code);
-if(flags.echo) {
-  source += ';'
+var source = "<?php \n\n" + toSource(code);
+if (flags.echo) {
+    source += ';'
 }
 
 console.log(source);
